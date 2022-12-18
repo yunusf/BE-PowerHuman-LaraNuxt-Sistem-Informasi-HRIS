36b96766375d51ae94685aea1ecc7b745f2da123
yunusf <yunusf42@gmail.com>
2022-12-18 18:11:19 +0700

    create relations to table User, Team, and Role

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -14,4 +14,22 @@ class Company extends Model
         'name',
         'logo',
     ];
+
+    // make relation with table user
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    // make relation with table team
+    public function teams()
+    {
+        return $this->hasMany(Team::class, 'company_id', 'id');
+    }
+
+    // make relation with table role
+    public function roles()
+    {
+        return $this->hasMany(Role::class, 'company_id', 'id');
+    }
 }
