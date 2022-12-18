9594a30d9eb31a80c077bd2002d6f15b286f12ca
yunusf <yunusf42@gmail.com>
2022-12-18 18:10:05 +0700

    create relation to table Company

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,10 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    // pembuatan relasi
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class);
+    }
 }
