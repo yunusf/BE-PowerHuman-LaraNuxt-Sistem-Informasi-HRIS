cd5eb92681bc2ad9527d744d70689764e6f1ef85
yunusf <yunusf42@gmail.com>
2022-12-20 21:27:25 +0700

    make relation many to one with team and role

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -22,4 +22,14 @@ class Employee extends Model
         'is_verified',
         'verified_at',
     ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
