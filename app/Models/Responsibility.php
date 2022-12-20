22fd29278d2f51bcf251c3f773b52029d14643b5
yunusf <yunusf42@gmail.com>
2022-12-20 20:34:59 +0700

    make relationship many to one with role

--- app/Models/Responsibility.php
+++ app/Models/Responsibility.php
@@ -13,6 +13,10 @@ class Responsibility extends Model
     protected $fillable = [
         'name',
         'role_id',
-
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
