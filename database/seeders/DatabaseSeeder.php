848c9de3e06f8aecf3e593c7ce7ac0072e86e0c1
yunusf <yunusf42@gmail.com>
2022-12-27 11:04:48 +0700

    menambahkan faker data untuk table team melalui factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Company;
+use App\Models\Team;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // membuat data faker untuk user
         // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory(10)->unverified()->create();
 
@@ -23,6 +25,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Company::factory(10)->create();
+        // membuat data faker untuk company
+        // Company::factory(10)->create();
+
+        // membuat data faker untuk team
+        Team::factory(30)->create();
     }
 }
