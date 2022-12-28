31d9242168d66d19d07b911330be32789cda3ee8
yunusf <yunusf42@gmail.com>
2022-12-28 14:22:00 +0700

    insert data dengan factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
 use App\Models\Role;
 use App\Models\Team;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -33,6 +35,12 @@ class DatabaseSeeder extends Seeder
         // Team::factory(30)->create();
 
         // membuat data faker untuk role
-        Role::factory(50)->create();
+        // Role::factory(50)->create();
+
+        // membuat data faker untuk responsibility
+        // Responsibility::factory(200)->create();
+
+        // membuat data faker untuk responsibility
+        Employee::factory(970)->create();
     }
 }
