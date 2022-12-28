89f2cbcd6a510913d4c926fddafd175c0ea716fa
yunusf <yunusf42@gmail.com>
2022-12-28 14:27:24 +0700

    create table hasil relasi user & company (m to m)

---
+++ database/migrations/2022_12_28_060703_create_user_company_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_company', function (Blueprint $table) {
+            $table->id();
+            // $table->timestamps();
+            $table->bigInteger('user_id')->unsigned(); // tidak terdapat - ataupun +
+            $table->bigInteger('company_id')->unsigned(); // tidak terdapat - ataupun +
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_company');
+    }
+};
