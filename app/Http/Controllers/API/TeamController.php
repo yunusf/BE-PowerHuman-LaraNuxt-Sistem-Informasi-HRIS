91946ed2632cd0db2ad276daa875d6514995bf4f
yunusf <yunusf42@gmail.com>
2023-01-06 18:19:12 +0700

    Make Team API

---
+++ app/Http/Controllers/API/TeamController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Exception;
+use App\Models\Team;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateTeamRequest;
+use App\Http\Requests\UpdateTeamRequest;
+
+class TeamController extends Controller
+{
+    //
+    public function fetch(Request $request)
+    {
+        $id = $request->input('id');
+        $name = $request->input('name');
+        $limit = $request->input('limit', 10);
+
+        $teamQuery = Team::query();
+
+        if ($id) {
+            $team = $teamQuery->find($id);
+
+            if ($team) {
+                return ResponseFormatter::success($team, 'Team Found');
+            }
+
+            return ResponseFormatter::error('Team Not Found!', 404);
+        }
+
+        $teams = $teamQuery->where('company_id', $request->company_id);
+
+        // filter pencarian
+        if ($name) {
+            $teams->where('name', 'like', '%' . $name . '%');
+        }
+
+        return ResponseFormatter::success($teams->paginate($limit), 'Teams Found');
+    }
+
+    public function create(CreateTeamRequest $request)
+    {
+        try {
+            // Upload Logo
+            if ($request->hasFile('icon')) {
+                $path = $request->file('icon')->store('public/icons');
+            }
+
+            // Create Team
+            $team = Team::create([
+                'name' => $request->name,
+                'icon' => $path,
+                'company_id' => $request->company_id,
+            ]);
+
+            if (!$team) {
+                throw new Exception('Team Not Created');
+            }
+
+            return ResponseFormatter::success($team, 'Team Created');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+
+    public function update(UpdateTeamRequest $request, $id)
+    {
+        try {
+            // get company
+            $team = Team::find($id);
+
+            // if company not found
+            if (!$team) {
+                throw new Exception('Team Not Found!');
+            }
+
+            //upload icon
+            if ($request->hasFile('icon')) {
+                $path = $request->file('icon')->store('public/icons');
+            }
+
+            // Update team
+            $team->update([
+                'name' => $request->name,
+                'icon' => $path,
+                'company' => $request->company,
+            ]);
+
+            return ResponseFormatter::success($team, 'Team Update');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            // get team
+            $team = Team::find($id);
+
+            // TODO: Check if team is owned by user!
+
+            // check if team exists
+            if (!$team) {
+                throw new Exception('Team Not Found!');
+            }
+
+            // Delete Team
+            $team->delete();
+            return ResponseFormatter::success('Team Deleted');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,9 +30,16 @@ Route::prefix('company')->middleware('auth:sanctum')->name('company.')->group(fu
     Route::post('update/{id}', [CompanyController::class, 'update'])->name('update'); // api.com/company
 });
 
-
 // AUTH|USER
 Route::post('login', [AuthController::class, 'login']); // kirim data => membuat token
 Route::post('register', [AuthController::class, 'register']); // kirim data => membuat token
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // kirim data => hapus token
 Route::get('user', [AuthController::class, 'fetch'])->middleware('auth:sanctum'); // get user data using token login/regis
+
+// TEAM
+Route::prefix('team')->middleware('auth:sanctum')->name('team')->group(function () {
+    Route::get('', [TeamController::class, 'fetch'])->name('fetch');
+    Route::post('', [TeamController::class, 'create'])->name('create');
+    Route::post('update/{id}', [TeamController::class, 'update'])->name('update');
+    Route::delete('{id}', [TeamController::class, 'destroy'])->name('delete');
+});
